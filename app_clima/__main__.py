a54cf9f1d05f4ab35d1a02838c8281dbccb7ee07
Vitor <figueiro22.22@gmail.com>
2026-06-29 23:50:18 -0400

    Criando o conteudo do arquivo main

---
+++ app_clima/__main__.py
@@ -0,0 +1,7 @@
+def main():
+    print('============================================')
+    print('A Sua Assistente de Clima! A sua disposição!')
+    print('============================================')
+
+if __name__ == '__main__':
+    main()
